7ac5eb5f45235e301e9b25362264d500e149c192
oliver <kunhan945@gmail.com>
2022-11-19 09:50:54 -0800

    adjust ProfileScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -7,33 +7,38 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-	return (
-		<View style={styles.container}>
-			<View style={styles.hero}>
-				<Text style={styles.greetings}>Greetings, Oliver</Text>
-			</View>
-			<View style={styles.calenderContainer}>
-				<Text>Here are some events that are happening</Text>
-			</View>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View style={styles.hero}>
+        <Text style={styles.greetings}>Greetings, Oliver</Text>
+      </View>
+      <View style={styles.calenderContainer}>
+        <Text style={styles.infoText}>
+          Here are some events that are happening
+        </Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-	},
-	greetings: {
-		fontSize: 20,
-		fontWeight: 'bold',
-	},
-	hero: {
-		padding: 20,
-		width: '100%',
-	},
-	calenderContainer: {
-		padding: 20,
-		width: '100%',
-	},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  greetings: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  hero: {
+    padding: 20,
+    width: '100%',
+  },
+  calenderContainer: {
+    padding: 20,
+    width: '100%',
+  },
+  infoText: {
+    fontSize: 20,
+  },
 });
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -2,42 +2,101 @@ import React from 'react';
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { Button } from 'react-native-elements';
 import { COLORS } from '../constants';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const { width, height } = Dimensions.get('window');
 
 const PROFILE_PLACEHOLDER = require('../assets/images/profile_placeholder.png');
 
+const UPVOTES = 0; // TODO: change this to read from db
+
 export default function ProfileScreen() {
-	return (
-		<View style={styles.container}>
-			<View>
-				<Image source={PROFILE_PLACEHOLDER} style={styles.avatar} />
-				<Text>Oliver</Text>
-				<Text>20123456</Text>
-				<Text>Computer Science</Text>
-			</View>
-			<Button title="Sign Out" buttonStyle={styles.button} />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={PROFILE_PLACEHOLDER} style={styles.avatar} />
+      </View>
+      <View style={styles.bannerArea}>
+        <Ionicons
+          name="heart"
+          size={30}
+          color={COLORS.grey}
+          style={{
+            marginRight: 5,
+          }}
+        />
+        {UPVOTES === 0 ? (
+          <Text style={styles.bannerText}>No upvotes yet</Text>
+        ) : (
+          <Text style={styles.bannerText}>{UPVOTES} upvotes</Text>
+        )}
+      </View>
+      <View style={styles.infoContainer}>
+        <View style={styles.singleLine}>
+          <Text style={styles.label}>User Name</Text>
+          <Text style={styles.infoText}>Oliver</Text>
+        </View>
+        <View style={styles.singleLine}>
+          <Text style={styles.label}>CWID</Text>
+          <Text style={styles.infoText}>20123456</Text>
+        </View>
+        <View style={styles.singleLine}>
+          <Text style={styles.label}>Major</Text>
+          <Text style={styles.infoText}>Computer Science</Text>
+        </View>
+      </View>
+      <Button title="Sign Out" buttonStyle={styles.button} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		paddingVertical: 20,
-	},
-	avatar: {
-		width: 150,
-		height: 150,
-		borderRadius: 75,
-		resizeMode: 'cover',
-		borderWidth: 3,
-		borderColor: COLORS.primary,
-	},
-	button: {
-		backgroundColor: COLORS.primary,
-		width: width - 40,
-	},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    resizeMode: 'cover',
+    borderWidth: 3,
+    borderColor: COLORS.primary,
+  },
+  button: {
+    backgroundColor: COLORS.primary,
+    width: width - 40,
+  },
+  bannerArea: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: COLORS.primary,
+    width: width,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  bannerText: {
+    color: COLORS.white,
+    fontSize: 20,
+  },
+  infoContainer: {
+    width: width - 40,
+    paddingVertical: 10,
+  },
+  singleLine: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  infoText: {
+    fontSize: 16,
+  },
 });
